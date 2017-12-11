5139d8d02951ac0bd76dce82ec23a43f0d10cb80
Doody Parizada <doody.parizada@gmail.com>
2017-12-11 15:34:21 +0200

    add python mock server

---
+++ server/app.py
@@ -0,0 +1,38 @@
+from flask import request, Flask, jsonify
+
+
+app = Flask(__name__)
+
+def init():
+    """
+    read the word vectors file.
+    init connection to DB.
+    """
+
+
+
+@app.route('/word/vector')
+def word_vectors():
+    ids = [i for i in request.args['ids'].split(',')]
+    return jsonify({'words': {i: {'vector': [0.1]*10} for i in ids}})
+
+
+@app.route('/spam/detect')
+def detect_spam():
+    import random
+    vector = request.args['vector'].split(',')
+    return jsonify({'spam': bool(random.random() > 0.8), 'confidence': 0.95})
+
+
+@app.route('/spam/report')
+def report_spam():
+    data = request.get_json()
+    vector = data['vector']
+    message = data['message']
+    return jsonify({})
+
+
+if __name__ == '__main__':
+    init()
+    app.run()
+
